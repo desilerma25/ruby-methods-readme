e37aa56f05cae3d6a03f224651efa99e97e24acc
Desiree Lerma <desi.lerma96@yahoo.com>
2020-10-19 17:39:52 -0500

    testing

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,40 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_hello_world_ten_times
+    phrase = "Hello World!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end
+
+say_hello_world_ten_times
+
+def greeting # Method Signature
+    puts "Hello World" #Method Body
+end #Method Closing
+
+def greeting 
+    # Leave a line break for the method body
+end # Immediately close the method
+
+def greeting
+    puts "Hello World" # Now code the body of the method
+end
+
+def greeting
+    putd "Hello World"
+end
+greeting # Executing the method by name
+#=> "Hello World"
+
+greeting # Executing the method again
+#=> "Hello World"
